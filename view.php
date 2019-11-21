debf095e5bb6e3530033b2ece13e6f3af5c1f344
noonedestroyer <tunagch176444@fpr.edu.vn>
2019-11-21 12:05:25 +0700

    Update view.php

--- view.php
+++ view.php
@@ -12,7 +12,14 @@
 <input type="button" value="ok"	onclick="display()">
 
 <script type="text/javascript">
+	function checkInput(){
+		var name = document.getElementById('name').value;
+		if (name.length <5){
+			alert('Name must be more than 5 characters');
+		}
+	}
 	function display(){
+		checkInput();
 		var name = document.getElementById('name').value;
 		var school = document.getElementById('school').value;
 		alert('Welcome ' + name + ' from school ' + school);

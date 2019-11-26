fdd6022cad58718e5f2cdb17da0368f34bcba31a
noonedestroyer <tunagch176444@fpr.edu.vn>
2019-11-26 11:31:25 +0700

    Update view.php

--- view.php
+++ view.php
@@ -1,27 +1,36 @@
-<!DOCTYPE html>
-<html>
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
 <body>
+	<?php
+		$db = parse_url(getenv("DATABASE_URL"));
 
-<h1>My first PHP page</h1>
-<p>Name: <input type="text" id="name"></p>
-
-<p>School: <input type="text" id="school"></p>
-
-
-<input type="button" value="ok"	onclick="display()">
-
-<script type="text/javascript">
-
-	function display(){
-		var name = document.getElementById('name').value;
-		var school = document.getElementById('school').value;
-		if (name.length <5){
-			alert('Name must be more than 5 characters');
-		}
-		else
-		alert('Welcome ' + name + ' from school ' + school);
-	}
-</script>
-
+		$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+		));
+		$sql = "seelct * from products";
+		//compile the sql
+		$stmt = $pdo->prepare($sql);
+		//execute the query
+		$stmt->setFetchMode(PDO::fetch_assoc);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+	?>
+	<ul>
+		<?php 
+			foreach ($resultSet as $row){
+				echo "<li>" . 
+					$row["name"] . "--" . $row["price"] . "</li>";
+			}
+		?>
+	</ul>
 </body>
 </html>

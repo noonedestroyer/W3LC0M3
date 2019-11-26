9f6edcdf7ce1caa5872fa313617ff59c4813b7a9
noonedestroyer <tunagch176444@fpr.edu.vn>
2019-11-26 11:40:38 +0700

    Update view.php

--- view.php
+++ view.php
@@ -1,36 +1,39 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-	<?php
-		$db = parse_url(getenv("DATABASE_URL"));
-
-		$pdo = new PDO("pgsql:" . sprintf(
-	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-	    $db["host"],
-	    $db["port"],
-	    $db["user"],
-	    $db["pass"],
-	    ltrim($db["path"], "/")
-		));
-		$sql = "seelct * from products";
-		//compile the sql
-		$stmt = $pdo->prepare($sql);
-		//execute the query
-		$stmt->setFetchMode(PDO::fetch_assoc);
-		$stmt->execute();
-		$resultSet = $stmt->fetchAll();
-	?>
-	<ul>
-		<?php 
-			foreach ($resultSet as $row){
-				echo "<li>" . 
-					$row["name"] . "--" . $row["price"] . "</li>";
-			}
-		?>
-	</ul>
+    <?php
+        //Refer to database 
+        $db = parse_url(getenv("DATABASE_URL"));
+        $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["host"],
+            $db["port"],
+            $db["user"],
+            $db["pass"],
+            ltrim($db["path"], "/")
+        ));    
+        $sql = "select * from products";
+        //compile the sql
+        $stmt = $pdo->prepare($sql);
+        //execute the query on the server and return the result set
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $resultSet = $stmt->fetchAll();
+    ?>
+    <ul>
+        <?php
+            foreach ($resultSet as $row) {
+                echo "<li>" .
+                    $row["name"] . '--'. $row["price"]
+                . "</li>";
+            }
+        ?>
+    </ul>
 </body>
 </html>

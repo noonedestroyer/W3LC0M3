0d9b22b2cbc230381964ce98d2904afa65a04b8f
noonedestroyer <tunagch176444@fpr.edu.vn>
2019-11-28 09:10:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-<a href="view.php">Profile</a>
-
+	<h1>Create Product </h1>
+	<form action="insertProduct.php" method="post">
+		Name <input type="text" name="name" >
+		<br>
+		Price <input type="text" name="price">
+		<br>
+		<input type="submit" value="Insert">
+	</form>
+	
 </body>
 </html>
